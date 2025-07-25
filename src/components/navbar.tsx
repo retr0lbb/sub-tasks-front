ef48c57842a8c22aab64b4c0e66b8a18f092b2ee
retr0lbb <retr0lbb@gmail.com>
2025-07-25 18:58:59 -0300

    feat: add dashboard layout

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+"use server"
+
+function Navbar(){
+    return (
+        <nav className="flex flex-1 min-w-20 max-w-40 bg-zinc-900 border border-zinc-200/10">
+
+        </nav>
+    )    
+}
+
+
+
+export { Navbar }
